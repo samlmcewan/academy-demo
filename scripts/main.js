42ae242998f8b5741b4f9008066dc795d1905122
samlmcewan <samlmcewan@gmail.com>
2021-08-19 13:33:58 +0100

    Fix banner toggle

--- scripts/main.js
+++ scripts/main.js
@@ -5,38 +5,54 @@ let banner2 = document.querySelector('#about');
 console.log(banner2);
 let banner3 = document.querySelector('#banner-photo');
 console.log(banner3);
-banner2.style.display = 'none';
-banner3.style.display = 'none';
+banner2.classList.add('hide');
+banner3.classList.add('hide');
+
 
 if (document.querySelector('input[name="banner-select"]')) {
     document.querySelectorAll('input[name="banner-select"]').forEach((elem) => {
       elem.addEventListener("change", function(event) {
+
+        banner2.classList.remove('hide');
+banner3.classList.remove('hide');
           
           
         var item = event.target.value;
         console.log(item);
 
-     
+    
         switch (item) {
             case 'banner-2':
-                banner2.style.display = 'block';
-                banner1.style.display = 'none';
-                banner3.style.display = 'none';
+                banner2.classList.remove('hide');
+                
+         
+                banner1.classList.add('hide');
+               
+                banner3.classList.add('hide');
               break;
               case 'banner-3':
-                banner3.style.display = 'block';
-                banner1.style.display = 'none';
-                banner2.style.display = 'none';
+                banner3.classList.remove('hide');
+  
+     
+                banner1.classList.add('hide');
+             
+                banner2.classList.add('hide');
               break;
               case 'banner-1':
-                banner1.style.display = 'block';
-                banner2.style.display = 'none';
-                banner3.style.display = 'none';
-            break;
+                banner1.classList.remove('hide');
+         
+              
+                banner2.classList.add('hide');
+           
+                banner3.classList.add('hide');
+              break;
             default:
-                banner1.style.display = 'block';
-                banner2.style.display = 'none';
-                banner3.style.display = 'none';
+                banner1.classList.remove('hide');
+         
+              
+                banner2.classList.add('hide');
+           
+                banner3.classList.add('hide');
           }
           
 
@@ -54,4 +70,4 @@ if (document.querySelector('input[name="banner-select"]')) {
 
       });
     });
-  }
+  } 
